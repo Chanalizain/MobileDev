c7fd3342a63135e9ac54b7fb4fc1911e7ffa61c2
Chanalizain <inchanaliza@gmail.com>
2025-10-29 12:17:03 +0700

    update firt stateless widget gradiant button

--- W3-S2/lib/Stateless-widget/main.dart
+++ W3-S2/lib/Stateless-widget/main.dart
@@ -2,79 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Column(
-//             children: [
-//               Container(
-//                 width: 300,
-//                 margin: EdgeInsets.all(30),
-//                 padding: EdgeInsets.all(40),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(30),
-//                   gradient: LinearGradient(
-//                   colors: [Colors.blue, Colors.purple])
-//                 ),
-//                 child: const Center(
-//                   child: Text(
-//                     'HELLO 1',
-//                     style: TextStyle(
-//                       fontSize: 35,
-//                       color: Colors.white
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Container(
-//                 width: 300,
-//                 padding: EdgeInsets.all(40),
-//                 margin: EdgeInsets.all(30),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(30),
-//                   gradient: LinearGradient(
-//                   colors: [Colors.blue, Colors.purple])
-//                 ),
-//                 child: const Center(
-//                   child: Text(
-//                     'HELLO 2',
-//                     style: TextStyle(
-//                       fontSize: 35,
-//                       color: Colors.white
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//               Container(
-//                 width: 300,
-//                 margin: EdgeInsets.all(30),
-//                 padding: EdgeInsets.all(40),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(30),
-//                   gradient: LinearGradient(
-//                   colors: [Colors.blue, Colors.purple])
-//                 ),
-//                 child: const Center(
-//                   child: Text(
-//                     'HELLO 3',
-//                     style: TextStyle(
-//                       fontSize: 35,
-//                       color: Colors.white
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              GradientButton(
+                text: "HELLO 1",
+                start: Colors.blue,
+                end: Colors.red, 
+              ),
+              GradientButton(
+                text: "HELLO 2",
+                start: Colors.blue,
+                end: Colors.red, 
+              ),
+              GradientButton(
+                text: "HELLO 3",
+                start: Colors.blue,
+                end: Colors.red, 
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
  }
 //st
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key, required this.text, required this.start, required this.end});
+class GradientButton extends StatelessWidget {
+  const GradientButton({super.key, required this.text, required this.start, required this.end});
   final String text;
   final Color start;
   final Color end;
