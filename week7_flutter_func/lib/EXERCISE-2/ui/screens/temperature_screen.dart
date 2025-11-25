29ab91a9e3ce77aa87b14a39ba4923456b503342
Chanalizain <inchanaliza@gmail.com>
2025-11-25 20:23:58 +0700

    temperature converter

--- week7_flutter_func/lib/EXERCISE-2/ui/screens/temperature_screen.dart
+++ week7_flutter_func/lib/EXERCISE-2/ui/screens/temperature_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class TemperatureScreen extends StatelessWidget {
-  TemperatureScreen({super.key});
+class TemperatureScreen extends StatefulWidget {
+  const TemperatureScreen({super.key}); 
+
+  @override
+  State<TemperatureScreen> createState() => _TemperatureScreenState();
+}
+
+class _TemperatureScreenState extends State<TemperatureScreen> {
+  String _inputDegrees = '';
+  String _fahrenheitResult = '0.0';
 
   final InputDecoration inputDecoration = InputDecoration(
     enabledBorder: OutlineInputBorder(
@@ -11,6 +19,21 @@ class TemperatureScreen extends StatelessWidget {
     hintText: 'Enter a temperature',
     hintStyle: const TextStyle(color: Colors.white),
   );
+
+  void _convertToFahrenheit(String value) {
+    setState(() {
+      _inputDegrees = value;
+      
+      final double? celsius = double.tryParse(value);
+
+      if (celsius == null) {
+        _fahrenheitResult = 'Invalid Input';
+      } else {
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        _fahrenheitResult = fahrenheit.toStringAsFixed(2); // Show 2 decimal places
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,22 +56,34 @@ class TemperatureScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 50),
-            const Text("Temperature in Degrees:"),
+            const Text("Temperature in Degrees (C):", style: TextStyle(color: Colors.white)),
             const SizedBox(height: 10),
+
             TextField(
               decoration: inputDecoration,
               style: const TextStyle(color: Colors.white),
+              keyboardType: TextInputType.number,
+              onChanged: _convertToFahrenheit, 
             ),
+            
             const SizedBox(height: 30),
-            const Text("Temperature in Fahrenheit:"),
+            const Text("Temperature in Fahrenheit (F):", style: TextStyle(color: Colors.white)),
             const SizedBox(height: 10),
+            
             Container(
               padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: const Text('test'),
+              child: Text(
+                _fahrenheitResult, 
+                style: const TextStyle(
+                  color: Colors.black, 
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ],
         ),
